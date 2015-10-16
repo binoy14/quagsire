e8465993b8fb45e24783e4a049f50b51a2c325c4
Binoy Patel <binoypatel14@gmail.com>
2015-10-16 11:27:42 -0400

    Write to text file and show id in ui

--- device.js
+++ device.js
@@ -1,23 +1,26 @@
 var device = require('./helpers/findDevice');
 var getId = require('./helpers/getId');
+var fs = require('fs');
 
 if(device){
   device.on('data', function(info){
+    document.getElementById('error').style.display = 'none';
     var hex = info.toString('hex');
     var cardDataHex = hex.split('');
     var finalId = getId(cardDataHex);
     if (finalId != false) {
-      console.log(finalId);
+      document.getElementById('student-id').innerHTML = finalId;
+      fs.appendFileSync('/Users/binoy/Documents/Five_Tech/College_Projects/ACM/attendence.txt', finalId + '\n');
     } else {
-      console.log('Please swipe again');
+      document.getElementById('error').style.display = 'block';
     }
   });
 } else {
-  console.log("Please connect the device");
+  document.getElementById('error-2').style.display = 'block';
 }
 
 device.on('error', function(err){
   if(err) throw err;
-  console.log("Please swipe again");
+  document.getElementById('error').style.display = 'block';
   device.close();
 })
